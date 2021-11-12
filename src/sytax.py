26668fdbda8e2046e34ab5305941be9a36981d28
kvrmnks <tayyx2000@163.com>
2021-11-12 16:30:56 +0800

    add readable list

--- src/sytax.py
+++ src/sytax.py
@@ -10,15 +10,16 @@ class Syntax:
         self.lexer = Lexer(code_path, "")
         self.grammar = Grammar(grammar)
         self.parser = LR1Parser(self.grammar)
-        # self.parser = SLRParser(grammar)
+        # self.parser = SLRParser(self.grammar)
         print(self.parser.G_indexed)
         self.parsing_table = self.parser.parse_table
         self.state_stack = [0]
+        self.readable_stack = []
 
     def process_one_hop(self, param: str):
         # print(self.parsing_table[self.state_stack[-1]])
         cmd = str(self.parsing_table[self.state_stack[-1]][param])
-        print(self.state_stack, cmd, param)
+        print(self.state_stack, cmd, param, self.readable_stack)
         if cmd == '':
             print("No such action!")
             exit(-1)
@@ -26,22 +27,25 @@ class Syntax:
             print('Finish!')
         elif cmd[0] == 's':
             self.state_stack.append(int(cmd[1:]))
+            self.readable_stack.append(param)
         elif cmd[0] == 'r':
             print(self.parser.G_indexed[int(cmd[1:])])
             tp = self.parser.G_indexed[int(cmd[1:])][1]  # A -> B, tp = B
             lp = self.parser.G_indexed[int(cmd[1:])][0]
             if tp[0] != '^':
                 self.state_stack = self.state_stack[0:len(self.state_stack) - len(tp)]
+                self.readable_stack = self.readable_stack[0: len(self.readable_stack) - len(tp)]
             self.process_one_hop(lp)
             self.process_one_hop(param)
         else:
             self.state_stack.append(int(cmd))
-
+            self.readable_stack.append(param)
     def process(self):
         while self.lexer.has_next():
             t = self.lexer.get_next()
             if t.token_type is None:
                 break
+            print('lexer', t.token_type, t.number, t.identifier)
             self.process_one_hop(t.token_type.value)
 
             # cmd = parsing_table[state_stack[-1]][t.token_type.value]
